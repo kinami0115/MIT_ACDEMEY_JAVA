b71061ba78ceff4fada2577c2fbe85f14b59e7e2
kinam <52563597+kinami0115@users.noreply.github.com>
2019-09-19 17:47:39 +0900

    Add files via upload

---
+++ Day-20190919_Third/test/logTest/LoggingSample1Test.java
@@ -0,0 +1,13 @@
+package logTest;
+
+import org.junit.Test;
+
+public class LoggingSample1Test {
+	
+	@Test
+	public void testInitLogging() {
+		LoggingSample1 ls = new LoggingSample1();
+		ls.initLogging();
+	}
+
+}
